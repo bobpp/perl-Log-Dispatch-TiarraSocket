aef9532d94939e3c5831fc60ff08a679867a2eeb
bobpp <bobpp.asroma+github@gmail.com>
2011-03-05 16:45:02 +0900

    Initialize commit skelton made by Module::Starter

---
+++ lib/Log/Dispatch/TiarraSocket.pm
@@ -0,0 +1,110 @@
+package Log::Dispatch::TiarraSocket;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Log::Dispatch::TiarraSocket - The great new Log::Dispatch::TiarraSocket!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Log::Dispatch::TiarraSocket;
+
+    my $foo = Log::Dispatch::TiarraSocket->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+bobpp, C<< <bobpp at bobpp.jp> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-log-dispatch-tiarrasocket at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Log-Dispatch-TiarraSocket>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Log::Dispatch::TiarraSocket
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Log-Dispatch-TiarraSocket>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Log-Dispatch-TiarraSocket>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Log-Dispatch-TiarraSocket>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Log-Dispatch-TiarraSocket/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 bobpp.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Log::Dispatch::TiarraSocket
